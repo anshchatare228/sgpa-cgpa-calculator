78f67b587158a1f9c1d2f42f20e794dd34b99cda
Ansh Chatare <ansh.chatare.228@gmail.com>
2026-04-25 20:20:21 +0530

    working on structure rendering function

--- main.js
+++ main.js
@@ -22,6 +22,30 @@ const curriculum = {
     }
 };
 
+
+const sem1 = document.getElementById("sem1");
+const sem2 = document.getElementById("sem2");
+const structA = document.getElementById("structA");
+const structB = document.getElementById("structB");
+const selectorPage = document.getElementById("selectorPage");
+const mainPage = document.getElementById("mainPage");
+
+function structSelected(selected){
+    selectorPage.classList.add("hidden");
+    mainPage.classList.remove("hidden");
+    
+    if(selected === "structA"){
+        renderStructA();
+    }
+
+    else{
+        // structB();
+    }
+    
+}
+
+
+
 // SGPA Calculation Function
 function calculateSGPA(semesterKey, studentGrades) {
     let totalPoints = 0;
@@ -36,29 +60,48 @@ function calculateSGPA(semesterKey, studentGrades) {
     return (totalPoints / semData.totalCredits).toFixed(2);
 }
 
-// Example: Handling M-2 (Maths-II) Logic
-function handleSubjectChange(selectedSubject) {
-    const pracSection = document.getElementById('practical-wrapper');
-    // If Mathematics-II is selected, hide practicals as it has 0 practical credits (L-3, T-0, P-0)
-    if (selectedSubject === "Mathematics-II (BSC-104)") {
-        pracSection.classList.add('hidden');
-    } else {
-        pracSection.classList.remove('hidden');
-    }
-}
-
-
-const structure = document.getElementById("structureSelect");
-const structureA = document.getElementById("structureA");
-const structureB = document.getElementById("structureB");
-structure.addEventListener("change",(selectedStructure)=>{
-    if(selectedStructure === "structureA"){
-        structureA.classList.remove("hidden")
-        structureB.classList.add("hidden");
-    }
+function renderStructA(){
+    const subjectRows = document.getElementById("subjectRows");
+    subjectRows.innerHTML = `
+    <select id='structureA' class='p-2 border rounded focus:ring-2 focus:ring-indigo-400 outline-none'>
+    <option value="" disabled selected hidden>Select Subject</option>
+    <option value="maths-2">M-1/M-2</option>
+    <option value="be">Chemistry</option>
+    <option value="bee">IKS</option>
+    <option value="physics">EGD</option>
+    <option value="pps">Drama</option>
+    <option value="exploration">ETM</option>
+    </select>
     
-    else{
-        structureB.classList.remove("hidden");
-        structureA.classList.add("hidden")
-    }
-})
+    <p class="md:hidden pt-1 mb-[-1rem]">Theory</p>
+    
+    <select class="theory-grade p-2 border rounded focus:ring-2 focus:ring-indigo-400">
+        <option value="" disabled selected hidden>Select Grade</option>
+        <option value="10">A+</option>
+        <option value="9">A</option>
+        <option value="8">B+</option>
+        <option value="7">A</option>
+        <option value="6">C+</option>
+        <option value="5">C</option>
+        <option value="4">D</option>
+        <option value="3">F</option>
+    </select>
+        
+    <p id="practical-wrapper" class="md:hidden pt-1 mb-[-1rem]">practical</p>
+
+    <select class="practical-grade p-2 border rounded focus:ring-2 focus:ring-indigo-400">
+        <option value="" disabled selected hidden>Select Grade</option>
+        <option value="10">A+</option>
+        <option value="9">A</option>
+        <option value="8">B+</option>
+        <option value="7">A</option>
+        <option value="6">C+</option>
+        <option value="5">C</option>
+        <option value="4">D</option>
+        <option value="3">F</option>
+    </select>
+        
+    <button id="removeBtn" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition mt-5 md:mt-0">Remove</button>
+    `
+}
+    
