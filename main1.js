5ea50a188952782e3c5bb726ffc9cbeff2ae5764
Ansh Chatare <ansh.chatare.228@gmail.com>
2026-05-08 12:58:34 +0530

    added theory logic

--- main1.js
+++ main1.js
@@ -6,43 +6,43 @@ bgVid.playbackRate = 0.5;
 const subjects = {
     sem1: {
         A: [
-            { name: "Maths-I", practical: false, credits: 4 },
-            { name: "Chemistry", practical: true, credits: 4 },
-            { name: "IKS", practical: true, credits: 4 },
-            { name: "ETM", practical: true, credits: 4 },
-            { name: "EGD", practical: true, credits: 4 },
-            { name: "PC", practical: true, credits: 4},
-            { name: "Drama/Photography", practical: true, credits: 2}
+            { name: "Maths-I",theory: true, practical: false, credits: 4 },
+            { name: "Chemistry",theory: true, practical: true, credits: 4 },
+            { name: "IKS", theory: true, practical: true, credits: 4 },
+            { name: "ETM", theory: true, practical: true, credits: 4 },
+            { name: "EGD",theory: true, practical: true, credits: 4 },
+            { name: "PC", theory: true, practical: true, credits: 4},
+            { name: "Drama/Photography", theory: false, practical: true, credits: 2}
         ],
 
         B: [
-            { name: "Maths-I", practical: false, credits: 4 },
-            { name: "Physics", practical: true, credits: 4 },
-            { name: "BEE", practical: true, credits: 4  },
-            { name: "BE", practical: true, credits: 4  },
-            { name: "PPS", practical: true, credits: 4  },
-            { name: "Yoga & Sports", practical: true, credits: 2  }
+            { name: "Maths-I",theory: true, practical: false, credits: 4 },
+            { name: "Physics",theory: true, practical: true, credits: 4 },
+            { name: "BEE",theory: true, practical: true, credits: 4  },
+            { name: "BE",theory: true, practical: true, credits: 4  },
+            { name: "PPS",theory: true, practical: true, credits: 4  },
+            { name: "Yoga & Sports", theory: false, practical: true, credits: 2  }
         ]
     },
 
     sem2: {
         A: [
-            { name: "Maths-II", practical: false, credits: 3 },
-            { name: "Chemistry", practical: true, credits: 4 },
-            { name: "IKS", practical: true, credits: 4 },
-            { name: "ETM", practical: true, credits: 4 },
-            { name: "EGD", practical: true, credits: 4 },
-            { name: "PC", practical: true, credits: 4},
-            { name: "Drama/Photography", practical: true, credits: 2}
+            { name: "Maths-II",theory: true, practical: false, credits: 3 },
+            { name: "Chemistry",theory: true, practical: true, credits: 4 },
+            { name: "IKS",theory: true, practical: true, credits: 4 },
+            { name: "ETM",theory: true, practical: true, credits: 4 },
+            { name: "EGD",theory: true, practical: true, credits: 4 },
+            { name: "PC",theory: true, practical: true, credits: 4},
+            { name: "Drama/Photography",theory: false, practical: true, credits: 2}
         ],
 
         B: [
-            { name: "Maths-II", practical: false, credits: 3 },
-            { name: "Physics", practical: true, credits: 4 },
-            { name: "BEE", practical: true, credits: 4  },
-            { name: "BE", practical: true, credits: 4  },
-            { name: "PPS", practical: true, credits: 4  },
-            { name: "Yoga & Sports", practical: true, credits: 2  }
+            { name: "Maths-II",theory: true, practical: false, credits: 3 },
+            { name: "Physics",theory: true, practical: true, credits: 4 },
+            { name: "BEE",theory: true, practical: true, credits: 4  },
+            { name: "BE",theory: true, practical: true, credits: 4  },
+            { name: "PPS",theory: true, practical: true, credits: 4  },
+            { name: "Yoga & Sports", theory: false, practical: true, credits: 2  }
         ]
     }
 };
@@ -88,17 +88,31 @@ function structSelected(structure) {
             <div class="font-semibold text-[1.3rem] text-gray-700 flex justify-center items-center">
                 ${subject.name}
             </div>
-            <span class="text-[0.95rem] text-black/60 font-light mb-[-1rem] md:hidden" >Theory</span>
-            <select class="theory-grade p-2 border rounded">
-                <option value="" disabled selected hidden>Select Grade</option>
-                <option value="10">O</option>
-                <option value="9">A+</option>
-                <option value="8">A</option>
-                <option value="7">B+</option>
-                <option value="6">B</option>
-                <option value="5">C</option>
-                <option value="0">F</option>
-            </select>
+
+            ${
+                subject.theory
+                ?
+                `
+                <span class="text-[0.95rem] text-black/60 font-light mb-[-1rem] md:hidden" >Practiclas</span>
+                <select class="practical-grade p-2 border rounded">
+                    <option value="" disabled selected hidden>Select Grade</option>
+                    <option value="9">A+</option>
+                    <option value="8">A</option>
+                    <option value="7">B+</option>
+                    <option value="6">B</option>
+                    <option value="5">C</option>
+                    <option value="4">D</option>
+                    <option value="0">F</option>
+                </select>
+                `
+                :
+                `
+                <div class="flex items-center text-gray-400 ml-10">
+                    No Theory
+                </div>
+                `
+
+            }
 
             ${
                 subject.practical
@@ -107,22 +121,24 @@ function structSelected(structure) {
                 <span class="text-[0.95rem] text-black/60 font-light mb-[-1rem] md:hidden" >Practiclas</span>
                 <select class="practical-grade p-2 border rounded">
                     <option value="" disabled selected hidden>Select Grade</option>
-                    <option value="10">O</option>
                     <option value="9">A+</option>
                     <option value="8">A</option>
                     <option value="7">B+</option>
                     <option value="6">B</option>
                     <option value="5">C</option>
+                    <option value="4">D</option>
                     <option value="0">F</option>
                 </select>
                 `
                 :
                 `
-                <div class="flex items-center text-gray-400">
+                <div class="flex items-center text-gray-400 ml-10">
                     No Practical
                 </div>
                 `
             }
+
+            
 
             <button class="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600">
                 Remove
