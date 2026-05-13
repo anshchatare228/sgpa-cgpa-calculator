2cdc64b022257bc5b3778b875224c14f1805b384
Ansh Chatare <ansh.chatare.228@gmail.com>
2026-05-13 18:09:39 +0530

    fixed issue

--- main1.js
+++ main1.js
@@ -100,7 +100,7 @@ function structSelected(structure) {
                 subject.theory
                 ?
                 `
-                <span class="text-[0.95rem] text-black/60 font-light mb-[-1rem] md:hidden" >Practiclas</span>
+                <span class="text-[0.95rem] text-black/60 font-light mb-[-1rem] md:hidden" >Theory</span>
                 <select class="theory-grade p-2 border rounded">
                     <option value="" disabled selected hidden>Select Grade</option>
                     <option value="9">A+</option>
